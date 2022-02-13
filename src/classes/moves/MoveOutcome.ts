4aa9197f5b2b02b88bf8c88eefe72d69b4ea87c5
rsek <hello@rsek.ca>
2022-02-13 15:18:49 -0800

    match results now a key of parent outcome type

--- src/classes/moves/MoveOutcome.ts
+++ src/classes/moves/MoveOutcome.ts
@@ -3,38 +3,37 @@ import { MdString } from "../general/MdString";
 
 export interface IMoveOutcomes extends IWillHaveId {
   $id?: string;
-  "Strong Hit with a Match"?: IMoveOutcome | undefined;
   "Strong Hit": IMoveOutcome;
   "Weak Hit": IMoveOutcome;
   "Miss": IMoveOutcome;
-  "Miss with a Match"?: IMoveOutcome | undefined;
 }
 
 export interface IMoveOutcome extends IWillHaveId {
   $id?: string;
   Text: MdString;
+  "With a Match"?: IMoveOutcome | undefined;
 }
 
 export class MoveOutcome implements IMoveOutcome, Omit<IHasId, "Name"> {
   $id: string;
-  Text!: string;
+  Text: MdString;
+  "With a Match"?: MoveOutcome | undefined;
   constructor(json: IMoveOutcome, id: string) {
     this.$id = id;
-    Object.assign(this, json);
+    this.Text = json.Text;
+    if (json["With a Match"]) {
+      this["With a Match"] = new MoveOutcome(json["With a Match"], `${this.$id} / With a Match`);
+    }
   }
 }
 
 export default class MoveOutcomes implements IMoveOutcomes, Omit<IHasId, "Name"> {
   $id: string;
-  "Strong Hit with a Match"?: IMoveOutcome | undefined;
   "Strong Hit": IMoveOutcome;
   "Weak Hit": IMoveOutcome;
   "Miss": IMoveOutcome;
-  "Miss with a Match"?: IMoveOutcome | undefined;
   constructor(json: IMoveOutcomes, id: string) {
     this.$id = id;
-    this["Strong Hit with a Match"] = new MoveOutcome(json["Strong Hit with a Match"] ?? json["Strong Hit"], `${this.$id} / Strong Hit with a Match`);
-    this["Miss with a Match"] = new MoveOutcome(json["Miss with a Match"] ?? json["Miss"], `${this.$id} / Miss with a Match`);
     this["Strong Hit"] = new MoveOutcome(json["Strong Hit"],`${this.$id} / Strong Hit`);
     this["Weak Hit"] = new MoveOutcome(json["Weak Hit"],`${this.$id} / Weak Hit`);
     this["Miss"] = new MoveOutcome(json["Miss"],`${this.$id} / Miss`);
