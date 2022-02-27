b4a68cf333a4ac82adf407fc4fd9f7096a4bb645
rsek <hello@rsek.ca>
2022-02-26 17:25:25 -0800

    rm unused dependencies, keys

--- src/types/gameobjects/planet/IPlanetData.ts
+++ src/types/gameobjects/planet/IPlanetData.ts
@@ -2,16 +2,11 @@ import LocationTheme from "../place/LocationTheme";
 import IPlaceData from "../IPlaceYaml";
 import Region from "../place/Region";
 import Atmosphere from "./Atmosphere";
-import Biome from "./Biome";
-import Diversity from "./Diversity";
 import Life from "./Life";
 import PlanetaryClass from "./PlanetaryClass";
-import Tuple from "../../general/Tuple";
 
 export default interface IPlanetData<R extends Region | undefined, P extends PlanetaryClass | undefined, LT extends LocationTheme | undefined> extends IPlaceData<R, undefined, LT> {
   "Planetary Class"?: P;
   Life?: Life | undefined;
   Atmosphere?: Atmosphere | undefined;
-  // Diversity: P extends PlanetaryClass.Vital ? ()Diversity : never;
-  // Biomes: P extends PlanetaryClass.Vital ? Tuple<Biome, this["Diversity"]> : never;
 }
