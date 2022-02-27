d937f75d4c43a859d0f899a457acbca6f23005ea
rsek <hello@rsek.ca>
2022-02-26 22:57:53 -0800

    minor content (022322); more precise sourcing

--- src/types/general/SourceTitle.ts
+++ src/types/general/SourceTitle.ts
@@ -1,7 +1,15 @@
 enum SourceTitle {
-  StarforgedBackerPreview = "Starforged Backer Preview",
   Starforged = "Ironsworn: Starforged Rulebook",
+  StarforgedBackerPreview = "Starforged Backer Preview",
+  StarforgedAssets = "Ironsworn: Starforged Assets",
   Ironsworn = "Ironsworn Rulebook",
-  IronswornDelve = "Ironsworn: Delve"
-} export default SourceTitle;
+  IronswornDelve = "Ironsworn: Delve",
+  IronswornAssets = "Ironsworn Assets",
+  IronswornBonusAssets = "Ironsworn Bonus Assets (July 2020)"
+}
+export default SourceTitle;
 
+enum SourceUrl {
+  IronswornBonusAssets = "https://drive.google.com/file/d/1bWyWxJzV_SVtyE_SeEGS4TMJ1ZBHfrdv/view",
+  IronswornRulebook = "https://www.ironswornrpg.com/downloads"
+}
