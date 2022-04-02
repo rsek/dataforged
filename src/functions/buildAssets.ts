5b03a0b841687d6a9f2ad694fb22e7f28551de8f
rsek <hello@rsek.ca>
2022-04-01 17:15:46 -0700

    tweaking imports, typecheck

--- src/functions/buildAssets.ts
+++ src/functions/buildAssets.ts
@@ -1,5 +1,5 @@
 import yaml from "js-yaml";
-import jsonpath from "jsonpath";
+import { apply } from "jsonpath";
 import fs from "fs";
 import getYamlFiles from "./io/getYamlFiles.js";
 import badJsonError from "./logging/badJsonError.js";
@@ -8,6 +8,7 @@ import Asset from "../types/assets/Asset.js";
 import type IAssetYaml from "../types/assets/interfaces/IAssetYaml.js";
 import type { AssetConditionMeterType } from "../types/general/ConditionMeter.js";
 import type ISource from "../types/general/interfaces/ISource.js";
+import type { RollableStat } from "../types/general/Stat.js";
 
 const assetPath = getYamlFiles().find(item => item.toString().match(/assets\.yaml$/)) as fs.PathLike;
 
@@ -16,7 +17,10 @@ interface assetDataRoot {
   Source: ISource;
   Assets: IAssetYaml[];
 }
-
+/**
+ * Build and validate all asset objects from YAML.
+ * @returns An array of Asset objects.
+ */
 export default function buildAssets() {
   const data = fs.readFileSync(assetPath, { encoding: "utf-8" });
   const json = yaml.load(data) as assetDataRoot;
@@ -24,7 +28,8 @@ export default function buildAssets() {
     return new Asset(assetData, json.Source);
   });
   result.forEach(asset => {
-    jsonpath.apply(asset, "$..Stat", stat => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    apply(asset, "$..Stat", (stat: RollableStat) => {
       if (stat === "Asset Condition Meter") {
         if (!asset["Condition Meter"]) {
           throw badJsonError(buildAssets, stat, "Asset references asset condition meter, but it doesn't have one.");
