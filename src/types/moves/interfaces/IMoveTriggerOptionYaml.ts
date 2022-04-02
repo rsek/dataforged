4e2aea0cf7064d5d675f6658a3196284d1e92e94
rsek <hello@rsek.ca>
2022-04-02 16:32:16 -0700

    import cleanup

--- src/types/moves/interfaces/IMoveTriggerOptionYaml.ts
+++ src/types/moves/interfaces/IMoveTriggerOptionYaml.ts
@@ -1,5 +1,5 @@
 import type MdString from "../../general/MdString.js";
-import type { IActionRoll, ICustomStat, IProgressRoll } from "../../general/Roll.js";
+import type { IActionRoll, IProgressRoll } from "../../general/Roll.js";
 
 export default interface IMoveTriggerOptionYaml {
   Text?: MdString | undefined;
