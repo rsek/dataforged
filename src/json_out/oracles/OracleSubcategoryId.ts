a39e84e204d0b41744636bd51d7e14ebfa6457dc
rsek <hello@rsek.ca>
2022-04-08 02:24:35 -0700

    export oracle name string types

--- src/json_out/oracles/OracleSubcategoryId.ts
+++ src/json_out/oracles/OracleSubcategoryId.ts
@@ -1,9 +1,9 @@
 
 import type { LocationTheme, OracleRoot, PlanetaryClass, VaultZone, Zone } from "@dataforged/json_out/index.js";
 
-type OracleSubcategoryPath = `Derelicts / ${Zone}` | `Location Themes / ${LocationTheme}` | `Planets / ${PlanetaryClass}` | `Vaults / ${VaultZone}`;
+export type OracleSubcategoryPath = `Derelicts / ${Zone}` | `Location Themes / ${LocationTheme}` | `Planets / ${PlanetaryClass}` | `Vaults / ${VaultZone}`;
 
 export type OracleSubcategoryName = PlanetaryClass | Zone | LocationTheme | VaultZone;
 
-type OracleSubcategoryId = `${OracleRoot} / ${OracleSubcategoryPath}`; export { OracleSubcategoryId };
+export type OracleSubcategoryId = `${OracleRoot} / ${OracleSubcategoryPath}`;
 
