c57dcef295a110bc3d7ceca43267b7f4d469775c
rsek <hello@rsek.ca>
2022-06-30 04:35:03 -0700

    update docs with reference to IOracleBase

--- dataforged-tools/src/json_out/oracles/IOracle.ts
+++ dataforged-tools/src/json_out/oracles/IOracle.ts
@@ -3,7 +3,8 @@ import type { IDisplayOracle , IOracleBase, IOracleCategory , IRow } from "@json
 /**
  * Represents an oracle, which may have a Table or multiple child Oracles.
  *
- * The distinction between {@link IOracleCategory} and IOracles that lack their own `Table` is a little arbitrary (and may be revised in the future).
+ * If you're looking for a way to crawl the oracle hierarchy in search of a specific ID, see {@link IOracleBase}.
+ *
  * @public
  */
 export interface IOracle extends IOracleBase {
@@ -15,5 +16,6 @@ export interface IOracle extends IOracleBase {
   Category: IOracleCategory["$id"];
   "Member of"?: IOracle["$id"] | undefined;
   "Table"?: IRow[] | undefined;
+  Categories: never;
 }
 
