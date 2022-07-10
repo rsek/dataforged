d24384d1220b3bf9fc91b53c3458005c51910afb
rsek <hello@rsek.ca>
2022-07-10 02:33:34 -0700

    rm import assertions for now

--- dist/esm-no-assert.js
+++
@@ -1,5 +0,0 @@
-import * as SF from "./starforged/dataforged.json";
-import * as IS from "./ironsworn/datasworn.json";
-const starforged = SF;
-const ironsworn = IS;
-export { starforged, ironsworn };
--- src/index-esm.ts
+++ src/index-esm.ts
@@ -1,6 +1,6 @@
 import type { Starforged, Ironsworn } from "./types";
-import SF from "./starforged/dataforged.json" assert {type: "json"};
-import IS from "./ironsworn/datasworn.json" assert {type: "json"};
+import * as SF from "./starforged/dataforged.json";
+import * as IS from "./ironsworn/datasworn.json";
 
 // for some reason these complain about ironsworn assets not having the right number of abilities. no Asset constructor throws an error for this on build, the schemas check out, and all IDs are numbered properly, so i don't know what that's about :shrug:
 
