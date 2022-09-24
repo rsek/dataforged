948acd4c8fd717ba8164291d79af592763304c00
rsek <hello@rsek.ca>
2022-09-24 05:37:35 -0700

    file rename to fit new scheme

--- dataforged-tools/src/schema_yaml/delve_site/index.ts
+++ dataforged-tools/src/schema_yaml/delve_site/index.ts
@@ -1,2 +1,2 @@
-export * from "@schema_yaml/delve_site/DelveCardYaml.js";
-export * from "@schema_yaml/delve_site/DelveSiteYaml.js";
+export * from "@schema_yaml/delve_site/YamlDelveCard.js";
+export * from "@schema_yaml/delve_site/YamlDelveSite.js";
