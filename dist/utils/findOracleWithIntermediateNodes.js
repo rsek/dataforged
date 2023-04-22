a9db558aceaa00f995c8684c87ff0bf94dfd4bf1
rsek <5354757+rsek@users.noreply.github.com>
2023-04-21 20:30:47 -0700

    restore oracle find fn

--- dist/utils/findOracleWithIntermediateNodes.js
+++ dist/utils/findOracleWithIntermediateNodes.js
@@ -7,15 +7,14 @@
 export function findOracleWithIntermediateNodes(id, oracleDataRoot) {
     const result = [];
     function walkCategory(cat) {
-        var _a, _b;
         result.push(cat);
         if (cat.$id === id)
             return true;
-        for (const oracle of (_a = cat.Oracles) !== null && _a !== void 0 ? _a : []) {
+        for (const oracle of cat.Oracles ?? []) {
             if (walkOracle(oracle))
                 return true;
         }
-        for (const childCat of (_b = cat.Categories) !== null && _b !== void 0 ? _b : []) {
+        for (const childCat of cat.Categories ?? []) {
             if (walkCategory(childCat))
                 return true;
         }
@@ -23,11 +22,10 @@ export function findOracleWithIntermediateNodes(id, oracleDataRoot) {
         return false;
     }
     function walkOracle(oracle) {
-        var _a;
         result.push(oracle);
         if (oracle.$id === id)
             return true;
-        for (const childOracle of (_a = oracle.Oracles) !== null && _a !== void 0 ? _a : []) {
+        for (const childOracle of oracle.Oracles ?? []) {
             if (walkOracle(childOracle))
                 return true;
         }
