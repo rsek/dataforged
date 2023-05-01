6cfe0b0d78feb61f66fd17796c82b0be1050318f
rsek <5354757+rsek@users.noreply.github.com>
2023-05-01 16:28:32 -0700

    recompile

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const starforged = require("./starforged/dataforged.json");
 const ironsworn = require("./ironsworn/datasworn.json");
 module.exports = { starforged, ironsworn };
+export {};
