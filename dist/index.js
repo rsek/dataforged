85f70628850339e01e75f369bcc653cbce516549
rsek <5354757+rsek@users.noreply.github.com>
2023-05-01 16:29:18 -0700

    build with correct options this time

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const starforged = require("./starforged/dataforged.json");
 const ironsworn = require("./ironsworn/datasworn.json");
 module.exports = { starforged, ironsworn };
-export {};
