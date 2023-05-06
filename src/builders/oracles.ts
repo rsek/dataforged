f0c918247512b8fa330611f32be98be0fa50149b
rsek <5354757+rsek@users.noreply.github.com>
2023-05-05 21:14:42 -0700

    clean up old code

--- src/builders/oracles.ts
+++
@@ -1,5 +0,0 @@
-// Ajv has several options that allow to modify data during validation:
-
-//     removeAdditional - to remove properties not defined in the schema object.
-//     useDefaults - to assign defaults from the schema to the validated data properties.
-//     coerceTypes - to change data type, when possible, to match the type(s) in the schema.
