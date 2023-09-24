ff423cf7c82d6bd9d5d17f9e0a7453835dc0e576
rsek <5354757+rsek@users.noreply.github.com>
2023-09-24 16:42:42 -0700

    add build script for `dist`

---
+++ src/scripts/build-dist.ts
@@ -0,0 +1,23 @@
+// Rebuilds the `dist` folder with the contents of the distributable package
+
+import fs from 'fs-extra'
+
+fs.emptyDirSync('dist')
+
+// copy json data
+fs.copySync('src/data-out/starforged', 'dist')
+
+// copy schema
+fs.copySync(
+	'src/data-out/starforged.schema.json',
+	'dist/starforged.schema.json'
+)
+
+// copy TS types to types dir
+fs.copySync('src/types/output/starforged.d.ts', 'dist/types.d.ts')
+
+// copy images
+fs.copySync('src/images', 'dist/images')
+
+// copy map of legacy IDs
+fs.copySync('src/legacy_id_map.json', 'dist/legacy_id_map.json')
